7c2588e34c00a35ff50627d76f36a71417ef709c
mahmoud saber <mahmoudsaber223015@gmail.com>
2023-12-09 15:41:21 +0200

    refactor

--- lib/core/api/stripe_service.dart
+++ lib/core/api/stripe_service.dart
@@ -2,12 +2,10 @@ import 'package:check_out_payment/core/api/api_keys.dart';
 import 'package:check_out_payment/core/api/dio_consumer.dart';
 import 'package:check_out_payment/features/checkout/data/models/payment_intent_input_model.dart';
 import 'package:check_out_payment/features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
-import 'package:check_out_payment/features/checkout/data/repositories/stripe_repo.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 
-class StripeRepoImpl implements StripeRepo {
+class StripeService {
   final DioConsumer dioConsumer = DioConsumer();
-  @override
   Future<PaymentIntentModel> createPaymentIntent(
       PaymentIntentInputModel paymentIntentInputModel) async {
     var response = await dioConsumer.postData(
@@ -18,7 +16,6 @@ class StripeRepoImpl implements StripeRepo {
     return paymentIntentModel;
   }
 
-  @override
   Future initPaymentSheet({required String paymentIntentClientSecret}) async {
     Stripe.instance.initPaymentSheet(
         paymentSheetParameters: SetupPaymentSheetParameters(
@@ -26,12 +23,10 @@ class StripeRepoImpl implements StripeRepo {
             merchantDisplayName: "Saber"));
   }
 
-  @override
   Future displayPaymentSheet() async {
     Stripe.instance.presentPaymentSheet();
   }
 
-  @override
   Future makePayment(
       {required PaymentIntentInputModel paymentIntentInputModel}) async {
     var paymentIntentModel = await createPaymentIntent(paymentIntentInputModel);
