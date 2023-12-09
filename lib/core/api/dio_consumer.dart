830529a486fee6529b3b98a9be0dc552930b3b72
mahmoud saber <mahmoudsaber223015@gmail.com>
2023-12-09 13:04:35 +0200

    create dio consumer

---
+++ lib/core/api/dio_consumer.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+
+class DioConsumer {
+  final Dio dio = Dio();
+
+  Future<Response> postData(
+      {required body,
+      required String url,
+      required String token,
+      String? contentType}) async {
+    Response response = await dio.post(url,
+        data: body,
+        options: Options(
+            contentType: contentType,
+            headers: {'Authorization': "Bearer $token"}));
+    return response;
+  }
+}
